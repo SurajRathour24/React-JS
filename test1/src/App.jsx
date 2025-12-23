1759e98713dd2f1f79efe4a06b3b888e368a4d06
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2025-12-23 22:16:54 +0530

    Tailwind css config

--- test1/src/App.jsx
+++ test1/src/App.jsx
@@ -3,12 +3,37 @@ import Navbar from './components/header/navbar'
 import Card from './components/Cards/card'
 
 const App = () => {
+  const students = [
+  {
+    id: 1,
+    name: "Aman Sharma",
+    age: 21,
+    course: "Computer Science",
+    city: "Chandigarh"
+  },
+  {
+    id: 2,
+    name: "Rohit Verma",
+    age: 22,
+    course: "Information Technology",
+    city: "Shimla"
+  },
+  {
+    id: 3,
+    name: "Neha Thakur",
+    age: 20,
+    course: "Electronics",
+    city: "Manali"
+  }
+];
   return (
     <div className='parent'>
       < Navbar />
 
       <div className="card-wrapper flex justify-between gap-x-2">
-        < Card />
+        {students.map(function(){
+          return < Card />
+        })}
       </div>
     </div>
   )
