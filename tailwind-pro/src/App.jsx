0388cd3348fa9420832d62b2346180f3f1562343
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-01-04 22:19:48 +0530

    Updated

---
+++ tailwind-pro/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="cards sm:flex justify-center align-middle space-x-3">
+        <div className="card-box w-32 h-32 bg-red-100 p-7 text-center ">Box1</div>
+        <div className="card-box w-32 h-32 bg-red-100 p-7 text-center ">Box1</div>
+        <div className="card-box w-32 h-32 bg-red-100 p-7 text-center ">Box1</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
