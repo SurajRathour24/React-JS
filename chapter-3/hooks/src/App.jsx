9a12e6e7e81553a3097163784a548455a93c8818
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-02-03 21:54:54 +0530

    Updated hooks

--- chapter-3/hooks/src/App.jsx
+++ chapter-3/hooks/src/App.jsx
@@ -2,18 +2,28 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  let [num, setnum] = useState(10);
+  const [num, setnum] = useState(10);
 
+  const [value, setvalue] = useState(0)
+
+  let increaseNum = () =>{
+    setvalue(value+1);
+  }
+  let decreaseNum = () =>{
+    if(value>0){
+      setvalue(value-1);
+    }
+  }
 
   return (
     <div>
-      <h1>Test</h1>
-      <h2>Value of num is {num}</h2>
-      <button onClick={()=>{
-        console.log(num);
-        setnum(num+1);
-        
-      }}>Change Num</button>
+      
+      <h1>{value}</h1>
+      <div className="btn-wrapper">
+      <button onClick={decreaseNum}>Decrease Num -</button>
+      <button onClick={increaseNum}>Increase Num +</button>
+      </div>
+
     </div>
   )
 }
