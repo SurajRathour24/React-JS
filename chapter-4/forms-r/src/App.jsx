9a241f1b6df40586b98b89f81af60fbfe863b032
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-02-04 09:11:58 +0530

    Updated

--- chapter-4/forms-r/src/App.jsx
+++ chapter-4/forms-r/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+
+  let submitHandler = (e) =>{
+    console.log("Form Submitted");
+    e.preventDefault();
+  }
+
+
+
+
   return (
     <div>
-      <form action="">
+      <form action="" onSubmit={submitHandler}>
 
-    <input type="text" placeholder='Enter Your Name' />
+    <input type="text" placeholder='Enter Your Name' onChange={function(e){
+      console.log(e.target.value);
+      
+    }} />
     <button> Submit Now </button>
 
       </form>
