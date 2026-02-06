1596328df883aa709a4e47e5d300e0ed60d4e25e
Suraj Rathour <110195289+SurajRathour24@users.noreply.github.com>
2026-02-06 23:18:30 +0530

    Updated

--- chapter-4/forms-r/src/App.jsx
+++ chapter-4/forms-r/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const App = () => {
 
+  const [input, setinput] = useState("")
 
   let submitHandler = (e) =>{
     console.log("Form Submitted");
@@ -15,8 +16,9 @@ const App = () => {
     <div>
       <form action="" onSubmit={submitHandler}>
 
-    <input type="text" placeholder='Enter Your Name' onChange={function(e){
-      console.log(e.target.value);
+    <input type="text" placeholder='Enter Your Name' value={input} onChange={function(e){
+      // console.log(e.target.value);
+      setinput(e.target.value);
       
     }} />
     <button> Submit Now </button>
